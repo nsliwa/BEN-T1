a51243a44a84ebfa828b623b494adb81f77a106d
ch484-mac4 <CH484-mac7@Guests-Mac-mini-2.local>
2015-01-26 22:16:27 -0600

    Initial Commit

---
+++ SMUExample1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SMUExample1
+//
+//  Created by ch484-mac4 on 1/26/15.
+//  Copyright (c) 2015 Team B.E.N. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
